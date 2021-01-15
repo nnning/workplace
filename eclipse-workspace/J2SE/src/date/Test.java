af58a8f21a364505d855c059db5c2592c60c6435
Zhang Ning <ningwazi@gmail.com>
2021-01-15 21:57:27 +0100

    test

---
+++ eclipse-workspace/J2SE/src/date/Test.java
@@ -0,0 +1,17 @@
+package date;
+
+import java.util.Date;
+
+public class Test {
+
+  public static void main(String[] args) {
+    
+    Date d = new Date();
+    System.out.println(d);
+
+    Date d1 = new Date(5000);
+    System.out.println(d1);
+  }
+
+  
+}
