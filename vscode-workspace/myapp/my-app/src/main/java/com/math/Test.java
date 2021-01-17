bf5e52ee04a24253083be10d0494bd11e52c5e8e
Zhang Ning <ningwazi@gmail.com>
2021-01-17 08:56:03 +0100

    test

---
+++ vscode-workspace/myapp/my-app/src/main/java/com/math/Test.java
@@ -0,0 +1,17 @@
+package com.math;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        double d = 36.87;
+
+        
+        System.out.println(Math.sin(Math.toRadians(d)));
+
+        double h = 12;
+        double o = h*Math.sin(Math.toRadians(d));
+        System.out.println(o);
+    }
+    
+}
